d4bc73b59232f3686fbc2a3bbffc2bc97560fd3f
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-09-08 12:06:35 +0545

    diplat form data in table

--- Personal Form (JavaScript)/script.js
+++ Personal Form (JavaScript)/script.js
@@ -1,67 +1,47 @@
-let submitForm = document.getElementById("submit-form");
-let inputName = document.getElementById("fname");
-let inputMiddleName = document.getElementById("mname");
-let inputLastName = document.getElementById("lname");
+let registrationForm = document.getElementById("registration-form");
+let inputName = document.getElementById("name");
+let inputAddress = document.getElementById("address");
 let inputEmail = document.getElementById("email");
-let inputContact = document.getElementById("contact");
-let inputAge = document.getElementById("age");
-let selectInputGender = document.getElementById("gender");
-let buttonSubmit = document.getElementById("submit");
-let buttonReset = document.getElementById("reset");
-let tableBody = document.getElementById("table-body");
+let inputNumber = document.getElementById("phone");
+let inputPassword = document.getElementById("password");
+let inputZipCode = document.getElementById("zip");
+let btnSubmit = document.getElementById("btn-submit");
 
+let tableBody = document.getElementById("table-body");
 tableBody.innerHTML = "";
+
 let submittedData = [];
 
-submitForm.addEventListener("submit", (event) => {
+registrationForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let formData = {
     enteredName: inputName.value,
-    enteredMiddleName: inputMiddleName.value,
-    enteredLastName: inputLastName.value,
+    enteredAddress: inputAddress.value,
     enteredEmail: inputEmail.value,
-    enteredContact: inputContact.value,
-    enteredAge: inputAge.value,
-    enteredSelectGender: selectInputGender.value,
+    enteredNumber: inputNumber.value,
+    enteredPassword: inputPassword.value,
+    enteredZipCode: inputZipCode.value,
   };
-
   submittedData.push(formData);
-
-  // Call displayData before resetting the form
   displayData();
-
-  // Reset the form after data is displayed
 });
-
-buttonReset.addEventListener("click", (event) => {
-  event.preventDefault();
-  resetForm();
-});
-
-function resetForm() {
-  inputName.value = "";
-  inputMiddleName.value = "";
-  inputLastName.value = "";
-  inputEmail.value = "";
-  inputContact.value = "";
-  inputAge.value = "";
-
-  // Reset selectInputGender to the first option
-  selectInputGender.selectedIndex = 0;
-}
 
 function displayData() {
   let html = "";
   submittedData.forEach(function (ele, index) {
-    html += `   <tr>
-            <td>${index + 1}</td>
-            <td>${ele.enteredName}</td>
-            <td>${ele.enteredMiddleName}</td>
-            <td>${ele.enteredLastName}</td>
-            <td>${ele.enteredEmail}</td>
-            <td>${ele.enteredAge}</td>
-            <td>${ele.enteredSelectGender}</td>
+    html += `<tr id="table-data">
+     <td>${index + 1}</td>
+            <td id="table-name">${ele.enteredName}</td>
+            <td id="table-address">${ele.enteredAddress}</td>
+            <td id="table-number">${ele.enteredNumber}</td>
+            <td id="table-email">${ele.enteredEmail}</td>
+            <td id="table-password">${ele.enteredPassword}</td>
+            <td id="table-zip">${ele.enteredZipCode}</td>
+            <td>
+              <button class="btn btn-primary">Edit</button>
+              <button class="btn btn-danger">Danger</button>
+            </td>
           </tr>`;
   });
   tableBody.innerHTML = html;
